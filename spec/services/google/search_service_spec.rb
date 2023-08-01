3c9ab33336160ac9cae55cb2a98d4440075c95ee
goose <purification1997@gmail.com>
2023-08-01 10:52:47 +0700

    Remove redundant tests

--- spec/services/google/search_service_spec.rb
+++ spec/services/google/search_service_spec.rb
@@ -6,20 +6,6 @@ require 'support/http_client_helpers'
 RSpec.describe Google::SearchService, type: :service do
   describe '#search!' do
     context 'when search a keyword' do
-      it 'perform a request against www.google.com' do
-        outer_env = nil
-        HttpClientHelpers.stub_http_adapter do |adapter, connection|
-          allow(described_class).to receive(:http_client).and_return(connection)
-          adapter.get('/search') do |env|
-            outer_env = env
-            200
-          end
-        end
-
-        described_class.search!('hello world')
-        expect(outer_env.url.host).to include('www.google.com')
-      end
-
       it 'returns the result page html when the request succeeds' do
         html = '<html><title>Search results</title></html>'
         HttpClientHelpers.stub_http_adapter do |adapter, connection|
