77490778458b6389e4af1cc565d529aa829ee7cc
goose <purification1997@gmail.com>
2023-08-02 12:53:30 +0700

    Add keyword model validation rules

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -6,4 +6,15 @@ class Keyword < ApplicationRecord
   has_many :keyword_search_entries, dependent: :destroy
 
   validates :content, :status, presence: true
+  validates :result_page_html, presence: true, if: :succeeded?
+  validates :links_count,
+            presence: true,
+            numericality: { greater_than_or_equal_to: 0 },
+            if: :succeeded?
+
+  private
+
+  def succeeded?
+    status == 'succeeded'
+  end
 end
