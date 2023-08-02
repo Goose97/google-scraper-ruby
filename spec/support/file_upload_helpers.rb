56cd6c12fd3134af9ece599f7f49b7c3eedf0021
goose <purification1997@gmail.com>
2023-08-02 15:35:04 +0700

    Use Rack::Test::UploadedFile to mock upload files

--- spec/support/file_upload_helpers.rb
+++ spec/support/file_upload_helpers.rb
@@ -2,10 +2,7 @@
 
 module FileUploadHelpers
   def self.upload_file(fixture:, content_type: 'text/csv')
-    ActionDispatch::Http::UploadedFile.new(
-      filename: fixture,
-      type: content_type,
-      tempfile: File.new(Rails.root.join('spec', 'fixtures', 'files', fixture))
-    )
+    path = Rails.root.join('spec', 'fixtures', 'files', fixture)
+    Rack::Test::UploadedFile.new(path, content_type)
   end
 end
