ebbe4040673ae00a9fbc65ef457ebbffd25ac453
goose <purification1997@gmail.com>
2023-08-09 16:09:32 +0700

    Submit form immediately after select file

---
+++ app/javascript/screens/keywords/index.js
@@ -0,0 +1,25 @@
+import CsvUploadForm from '../../components/csv_upload_form';
+
+const SELECTOR = {
+  screen: '.screen-keyword',
+  uploadForm: '.form-csv-upload',
+};
+
+class KeywordScreen {
+  constructor(screenElement) {
+    this.screenElement = screenElement;
+    this._setup();
+  }
+
+  _setup() {
+    const form = this.screenElement.querySelector(SELECTOR.uploadForm);
+    new CsvUploadForm(form);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const keywordScreen = document.querySelector(SELECTOR.screen);
+  if (keywordScreen) {
+    new KeywordScreen(keywordScreen);
+  }
+});
