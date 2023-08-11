d93acb0abbf3765eceda45d0cc3316b982af5365
goose <purification1997@gmail.com>
2023-08-11 11:37:24 +0700

    Change NOTE to TODO

--- app/queries/keywords_query.rb
+++ app/queries/keywords_query.rb
@@ -1,13 +1,8 @@
 # frozen_string_literal: true
 
 class KeywordsQuery
-  def initialize(filters = {})
-    # NOTE(Goose97): we don't support any filters for now
-    @filters = filters
-  end
-
   def call
-    # NOTE(Goose97): the details information about keywords will be implement
+    # TODO(Goose97): the details information about keywords will be implement
     # in view keyword details ticket (#21)
     Keyword.order(content: :asc)
   end
