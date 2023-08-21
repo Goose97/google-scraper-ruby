b55c8f986c755e4cfa50f22f26e118f1474428fb
goose <purification1997@gmail.com>
2023-08-21 10:45:50 +0700

    Create ApplicationSerializer base

--- app/serializers/v1/keyword_serializer.rb
+++ app/serializers/v1/keyword_serializer.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 module V1
-  class KeywordSerializer
-    include JSONAPI::Serializer
-
+  class KeywordSerializer < ApplicationSerializer
     attributes :content, :status, :created_at, :updated_at
   end
 end
