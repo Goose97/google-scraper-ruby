e3427e9c9766210577b1b1867b659a1617d53ef8
goose <purification1997@gmail.com>
2023-08-23 07:58:43 +0700

    Move decorator logic into query object

--- app/queries/keyword_search_entries_query.rb
+++ app/queries/keyword_search_entries_query.rb
@@ -23,7 +23,19 @@ class KeywordSearchEntriesQuery
     relation.where(kind: :non_ads).size
   end
 
+  def top_ads_urls
+    urls_of(relation.where(kind: :ads, position: :top))
+  end
+
+  def non_ads_urls
+    urls_of(relation.where(kind: :non_ads))
+  end
+
   private
 
   attr_reader :relation
+
+  def urls_of(query)
+    query.pluck(:urls).flatten.uniq
+  end
 end
