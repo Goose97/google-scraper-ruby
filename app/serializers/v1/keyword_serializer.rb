94a8558a10fb388a0aede7696441c70800da97ea
goose <purification1997@gmail.com>
2023-08-29 10:28:52 +0700

    Simplify response structure

--- app/serializers/v1/keyword_serializer.rb
+++ app/serializers/v1/keyword_serializer.rb
@@ -4,14 +4,24 @@ module V1
   class KeywordSerializer < V1::KeywordSimpleSerializer
     attributes :result_page_html, :links_count
 
-    attributes :search_entries_info do |_, params|
-      {
-        top_ads_count: params[:search_entries_query].top_ads_count,
-        total_ads_count: params[:search_entries_query].total_ads_count,
-        non_ads_count: params[:search_entries_query].non_ads_count,
-        top_ads_urls: params[:search_entries_query].top_ads_urls,
-        non_ads_urls: params[:search_entries_query].non_ads_urls
-      }
+    attributes :top_ads_count do |_, params|
+      params[:search_entries_query].top_ads_count
+    end
+
+    attributes :total_ads_count do |_, params|
+      params[:search_entries_query].total_ads_count
+    end
+
+    attributes :non_ads_count do |_, params|
+      params[:search_entries_query].non_ads_count
+    end
+
+    attributes :top_ads_urls do |_, params|
+      params[:search_entries_query].top_ads_urls
+    end
+
+    attributes :non_ads_urls do |_, params|
+      params[:search_entries_query].non_ads_urls
     end
   end
 end
