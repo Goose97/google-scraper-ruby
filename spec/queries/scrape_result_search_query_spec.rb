896c811c0b403d37f9510e02db1d36317aa07a6e
goose <purification1997@gmail.com>
2023-08-29 09:44:58 +0700

    Group result by keyword_id

--- spec/queries/scrape_result_search_query_spec.rb
+++ spec/queries/scrape_result_search_query_spec.rb
@@ -5,41 +5,45 @@ require 'rails_helper'
 RSpec.describe(ScrapeResultSearchQuery) do
   describe '#call' do
     context 'given :exact query type' do
-      it 'returns all URLs satisfy the query pattern and their corresponding keyword_id' do
+      it 'returns all URLs satisfy the query pattern grouped by their keyword_id' do
         url = 'https://google.com'
         keyword = Fabricate(:parsed_keyword) { keyword_search_entries(count: 0) }
-        Fabricate(:keyword_search_entry, urls: [url], keyword_id: keyword.id)
+        Fabricate.times(2, :keyword_search_entry, urls: [url], keyword_id: keyword.id)
         Fabricate(:keyword_search_entry, urls: ['https://ruby-lang.org'], keyword_id: keyword.id)
 
         result = described_class.new(query_type: :exact, pattern: url).call
 
-        expect(result).to(contain_exactly({ keyword_id: keyword.id, urls: [url] }))
+        expect(result).to(contain_exactly({ keyword_id: keyword.id, urls: [url, url] }))
       end
     end
 
     context 'given :partial query type' do
-      it 'returns all URLs satisfy the query pattern and their corresponding keyword_id' do
-        url = 'https://google.com'
+      it 'returns all URLs satisfy the query pattern grouped by their keyword_id' do
+        url_a = 'https://google.com'
+        url_b = 'https://www.google.com'
         keyword = Fabricate(:parsed_keyword) { keyword_search_entries(count: 0) }
-        Fabricate(:keyword_search_entry, urls: [url], keyword_id: keyword.id)
+        Fabricate(:keyword_search_entry, urls: [url_a], keyword_id: keyword.id)
+        Fabricate(:keyword_search_entry, urls: [url_b], keyword_id: keyword.id)
         Fabricate(:keyword_search_entry, urls: ['https://ruby-lang.org'], keyword_id: keyword.id)
 
         result = described_class.new(query_type: :partial, pattern: 'google').call
 
-        expect(result).to(contain_exactly({ keyword_id: keyword.id, urls: [url] }))
+        expect(result).to(contain_exactly({ keyword_id: keyword.id, urls: [url_a, url_b] }))
       end
     end
 
     context 'given :pattern query type' do
-      it 'returns all URLs satisfy the query pattern and their corresponding keyword_id' do
-        url = 'https://google.com'
+      it 'returns all URLs satisfy the query pattern grouped by their keyword_id' do
+        url_a = 'https://google.com'
+        url_b = 'https://www.google.com'
         keyword = Fabricate(:parsed_keyword) { keyword_search_entries(count: 0) }
-        Fabricate(:keyword_search_entry, urls: [url], keyword_id: keyword.id)
+        Fabricate(:keyword_search_entry, urls: [url_a], keyword_id: keyword.id)
+        Fabricate(:keyword_search_entry, urls: [url_b], keyword_id: keyword.id)
         Fabricate(:keyword_search_entry, urls: ['https://ruby-lang.org'], keyword_id: keyword.id)
 
         result = described_class.new(query_type: :pattern, pattern: 'go{2}\w+\.com$').call
 
-        expect(result).to(contain_exactly({ keyword_id: keyword.id, urls: [url] }))
+        expect(result).to(contain_exactly({ keyword_id: keyword.id, urls: [url_a, url_b] }))
       end
     end
 
