e113db47648a5d9a98f395f10ae4aaed4cd56df9
goose <purification1997@gmail.com>
2023-08-30 11:07:21 +0700

    Add system spec

---
+++ spec/systems/keywords/search_scrape_result_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'Search scrape result', type: :system do
+  context 'given VALID params' do
+    it 'displays search result' do
+      keyword = Fabricate(:parsed_keyword) { keyword_search_entries(count: 0) }
+      Fabricate(:keyword_search_entry, urls: ['https://ruby-lang.org', 'https://ruby-doc.org'], keyword_id: keyword.id)
+
+      visit(search_keywords_path({ search: 'ruby', query_type: 'partial' }))
+
+      expect(find("[data-testid='search_result_total_matched_urls']")).to(have_content(2))
+
+      within("div[data-keyword-id='#{keyword.id}']") do |item|
+        item.assert_selector("ul[data-testid='search_result_url_list'] > li", count: 2)
+      end
+    end
+  end
+end
