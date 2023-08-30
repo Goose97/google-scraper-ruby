cd0e77a6aca9218c8298af7280255cb878402ccc
Nguyễn Văn Đức <35915460+Goose97@users.noreply.github.com>
2023-08-30 16:56:33 +0700

    [API] As a User, I can get the details of my keywords (#40)

--- app/serializers/v1/keyword_serializer.rb
+++ app/serializers/v1/keyword_serializer.rb
@@ -1,7 +1,27 @@
 # frozen_string_literal: true
 
 module V1
-  class KeywordSerializer < ApplicationSerializer
-    attributes :content, :status, :created_at, :updated_at
+  class KeywordSerializer < V1::KeywordSimpleSerializer
+    attributes :result_page_html, :links_count
+
+    attributes :top_ads_count do |_, params|
+      params[:search_entries_query].top_ads_count
+    end
+
+    attributes :total_ads_count do |_, params|
+      params[:search_entries_query].total_ads_count
+    end
+
+    attributes :non_ads_count do |_, params|
+      params[:search_entries_query].non_ads_count
+    end
+
+    attributes :top_ads_urls do |_, params|
+      params[:search_entries_query].top_ads_urls
+    end
+
+    attributes :non_ads_urls do |_, params|
+      params[:search_entries_query].non_ads_urls
+    end
   end
 end
