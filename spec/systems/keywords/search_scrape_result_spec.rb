57a8b217a80abd0f9bf1d2ccd5dabeee9f72c58b
goose <purification1997@gmail.com>
2023-08-30 11:42:22 +0700

    Shorten test case

--- spec/systems/keywords/search_scrape_result_spec.rb
+++ spec/systems/keywords/search_scrape_result_spec.rb
@@ -4,20 +4,22 @@ require 'rails_helper'
 
 describe 'Search scrape result', type: :system do
   context 'given VALID params' do
+    # rubocop:disable RSpec/NoExpectationExample
     it 'displays search result' do
       keyword = Fabricate(:parsed_keyword) { keyword_search_entries(count: 0) }
       Fabricate(:keyword_search_entry, urls: ['https://ruby-lang.org', 'https://ruby-doc.org'], keyword_id: keyword.id)
 
       visit(search_keywords_path({ search: 'ruby', query_type: 'partial' }))
 
-      expect(find("[data-testid='search_result_search_term']")).to(have_content('ruby'))
-      expect(find("[data-testid='search_result_query_type']")).to(have_content('partial'))
-      expect(find("[data-testid='search_result_total_matched_urls']")).to(have_content(2))
-      expect(find("[data-testid='search_result_keyword_content']")).to(have_content(keyword.content))
+      page.assert_selector("[data-testid='search_result_search_term']", text: 'ruby', count: 1)
+      page.assert_selector("[data-testid='search_result_query_type']", text: 'partial', count: 1)
+      page.assert_selector("[data-testid='search_result_total_matched_urls']", text: '2', count: 1)
 
       within("div[data-keyword-id='#{keyword.id}']") do |item|
+        item.assert_selector("[data-testid='search_result_keyword_content']", text: keyword.content, count: 1)
         item.assert_selector("ul[data-testid='search_result_url_list'] > li", count: 2)
       end
     end
+    # rubocop:enable RSpec/NoExpectationExample
   end
 end
