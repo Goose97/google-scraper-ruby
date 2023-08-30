304f377a67ee93e098947cc408edc6e6441ce275
goose <purification1997@gmail.com>
2023-08-30 16:55:44 +0700

    Properly test users interaction flow

--- spec/systems/keywords/search_scrape_result_spec.rb
+++ spec/systems/keywords/search_scrape_result_spec.rb
@@ -4,12 +4,18 @@ require 'rails_helper'
 
 describe 'Search scrape result', type: :system do
   context 'given VALID params' do
-    # rubocop:disable RSpec/NoExpectationExample
+    # rubocop:disable RSpec/NoExpectationExample, RSpec/ExampleLength
     it 'displays search result' do
       keyword = Fabricate(:parsed_keyword) { keyword_search_entries(count: 0) }
       Fabricate(:keyword_search_entry, urls: ['https://ruby-lang.org', 'https://ruby-doc.org'], keyword_id: keyword.id)
 
-      visit(search_keywords_path({ search: 'ruby', query_type: 'partial' }))
+      visit(root_path)
+      within('form[data-testid="scrape-result-search-form"]') do
+        fill_in('search', with: 'ruby')
+        select(I18n.t('keywords.query_type.partial'), from: 'query_type')
+
+        click_button I18n.t('shared.submit_search')
+      end
 
       page.assert_selector("[data-testid='search_result_search_term']", text: 'ruby', count: 1)
       page.assert_selector("[data-testid='search_result_query_type']", text: 'partial', count: 1)
@@ -20,6 +26,6 @@ describe 'Search scrape result', type: :system do
         item.assert_selector("ul[data-testid='search_result_url_list'] > li", count: 2)
       end
     end
-    # rubocop:enable RSpec/NoExpectationExample
+    # rubocop:enable RSpec/NoExpectationExample, RSpec/ExampleLength
   end
 end
