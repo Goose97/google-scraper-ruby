15223d84800492f1918f059b6096daf122eaebc9
goose <purification1997@gmail.com>
2023-08-31 16:32:39 +0700

    Improve test case

--- spec/systems/keywords/search_scrape_result_spec.rb
+++ spec/systems/keywords/search_scrape_result_spec.rb
@@ -6,8 +6,10 @@ describe 'Search scrape result', type: :system do
   context 'given VALID params' do
     # rubocop:disable RSpec/NoExpectationExample, RSpec/ExampleLength
     it 'displays search result' do
-      keyword = Fabricate(:parsed_keyword) { keyword_search_entries(count: 0) }
-      Fabricate(:keyword_search_entry, urls: ['https://ruby-lang.org', 'https://ruby-doc.org'], keyword_id: keyword.id)
+      keyword_a = Fabricate(:parsed_keyword) { keyword_search_entries(count: 0) }
+      keyword_b = Fabricate(:parsed_keyword) { keyword_search_entries(count: 0) }
+      Fabricate(:keyword_search_entry, urls: ['https://ruby-lang.org', 'https://ruby-doc.org'], keyword_id: keyword_a.id)
+      Fabricate(:keyword_search_entry, urls: ['https://rubyapi.org'], keyword_id: keyword_b.id)
 
       visit(root_path)
       within('form[data-testid="scrape-result-search-form"]') do
@@ -19,11 +21,16 @@ describe 'Search scrape result', type: :system do
 
       page.assert_selector("[data-testid='search_result_search_term']", text: 'ruby', count: 1)
       page.assert_selector("[data-testid='search_result_query_type']", text: 'partial', count: 1)
-      page.assert_selector("[data-testid='search_result_total_matched_urls']", text: '2', count: 1)
+      page.assert_selector("[data-testid='search_result_total_matched_urls']", text: '3', count: 1)
 
-      within("div[data-keyword-id='#{keyword.id}']") do |item|
-        item.assert_selector("[data-testid='search_result_keyword_content']", text: keyword.content, count: 1)
+      within("div[data-keyword-id='#{keyword_a.id}']") do |item|
+        item.assert_selector("[data-testid='search_result_keyword_content']", text: keyword_a.content, count: 1)
         item.assert_selector("ul[data-testid='search_result_url_list'] > li", count: 2)
+      end
+
+      within("div[data-keyword-id='#{keyword_b.id}']") do |item|
+        item.assert_selector("[data-testid='search_result_keyword_content']", text: keyword_b.content, count: 1)
+        item.assert_selector("ul[data-testid='search_result_url_list'] > li", count: 1)
       end
     end
     # rubocop:enable RSpec/NoExpectationExample, RSpec/ExampleLength
