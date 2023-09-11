333c9310fb5607fd102affc3a065b388338c98d9
goose <purification1997@gmail.com>
2023-09-11 15:10:46 +0700

    Move AlertHelper into ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@
 
 module ApplicationHelper
   include Pagy::Frontend
+
+  def flash_alert
+    # Normalize flash[:alert] since it can be either an array of message or a single message
+    # For example, devise also use flash[:alert] to display error message and pass a single message
+    alert = flash[:alert]
+    return alert if alert.is_a?(Array)
+
+    [alert]
+  end
 end
